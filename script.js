9876ce8ddd3ea9e6af2c068c46649c2f3ca9d8dc
Hisham <linkedin.hisham@gmail.com>
2025-08-19 11:06:04 +0530

    fixed the mobile handburger dropdown styles , desktop nav bar, added functionality to dismiss navbar when clicking anywhere on screen,  added hover effects for buttons

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function toggleNavBar() {
-
     const navLinks = document.getElementById("nav-items");
     navLinks.classList.toggle("active");
-} 
+}
+
+/* closing drawer when clicking outside of the screen */
+document.addEventListener("click", function (event) {
+    const navLinks = document.getElementById("nav-items");
+    const handburger = document.querySelector(".hamburger");
+
+    if (navLinks.classList.contains("active") &&
+     !navLinks.contains(event.target) /* not clicked nav items */ &&
+      !handburger.contains(event.target /* not clicked handburger */
+
+      )) {
+        navLinks.classList.remove("active");
+    }
+})
