6851b38ae06fa4dcec5631c14808c7284693c501
Hisham <linkedin.hisham@gmail.com>
2025-08-19 14:38:56 +0530

    added functionality to move carousel slider,  changed button send from img to styled button, added hover effected on arrows and navigation buttons, used transitions instead of animations

--- script.js
+++ script.js
@@ -9,10 +9,27 @@ document.addEventListener("click", function (event) {
     const handburger = document.querySelector(".hamburger");
 
     if (navLinks.classList.contains("active") &&
-     !navLinks.contains(event.target) /* not clicked nav items */ &&
-      !handburger.contains(event.target /* not clicked handburger */
-
-      )) {
+        !navLinks.contains(event.target) /* not clicked nav items */ &&
+        !handburger.contains(event.target)/* not clicked handburger */) {
         navLinks.classList.remove("active");
     }
 })
+
+/* adding scroll effect on slides */
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rightBtn = document.querySelector(".get-started .slide-navigations .right");
+    const leftBtn = document.querySelector(".get-started .slide-navigations .left");
+    const carouselSlider = document.querySelector(".slides");
+
+    const scrollAmount = 150; // pixels
+
+    rightBtn.addEventListener("click", () => {
+        carouselSlider.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+
+    leftBtn.addEventListener("click", () => {
+        carouselSlider.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+});
+
